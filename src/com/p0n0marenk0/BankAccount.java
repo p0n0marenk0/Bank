eee6257bff4bf4ccbdcdb13042b13020ad908e67
Serhii Ponomarenko <ponomarenkoss99@gmail.com>
2021-03-24 22:14:40 +0200

    Final Commit

--- src/com/p0n0marenk0/BankAccount.java
+++ src/com/p0n0marenk0/BankAccount.java
@@ -12,12 +12,33 @@ package com.p0n0marenk0;
 
         // 0.5% commission
         public void add(double money) {
-            //count += 99.5
+            count += money-money*0.5/100;
         }
 
         // 1% commission
         public void transfer(BankAccount otherPerson, double money){
+            if (count >= money ){
+                count = count - money;
+            }else{
+                System.out.println(otherPerson.getName() + " Ой, давайте завтра рассчитаюсь)");
+            }
+            otherPerson.count += money-money*1/100;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getCount() {
+            return count;
+        }
+
+        public void setCount(double count) {
+            this.count = count;
         }
     }
 
--- src/com/p0n0marenk0/Main.java
+++ src/com/p0n0marenk0/Main.java
@@ -10,12 +10,17 @@ public class Main {
         BankAccount sergey = new BankAccount("Sergey", 0);
 
         boss.add(100000);
+        System.out.println(boss.getCount());
+        boss.add(300000);
+        System.out.println(boss.getCount());
 
-        boss.transfer(ivan, 10000);
+
+        boss.transfer(ivan, 9000);
         boss.transfer(alex, 15000);
         boss.transfer(sergey, 20000);
-
-        // System.out.println();
-
+        System.out.println(ivan.getName() + ": " + ivan.getCount());
+        System.out.println(alex.getName() + ": " + alex.getCount());
+        System.out.println(sergey.getName() + ": " + sergey.getCount());
+        System.out.println(boss.getName() + ": " + boss.getCount());
     }
 }
